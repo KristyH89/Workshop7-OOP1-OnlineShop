09c247cc912a0d13fb904acc85ff57f60c7e0b6e
Kristy Heijenk <ksheijenk@hotmail.com>
2026-03-19 15:52:13 +0100

    Added order

--- src/main/java/org/onlineshop/domain/Order.java
+++ src/main/java/org/onlineshop/domain/Order.java
@@ -1,4 +1,47 @@
 package org.onlineshop.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Order {
+
+    private final Customer customer;
+    private final List<Product> products;
+    private double totalPrice;
+
+    // Constructor
+    public Order(Customer customer) {
+        if (customer == null) {
+            throw new IllegalArgumentException("Customer cannot be null");
+        }
+
+        this.customer = customer;
+        this.products = new ArrayList<>();
+        this.totalPrice = 0;
+    }
+    // Add product and automatic update price
+    public void addProduct(Product p) {
+        products.add(p);
+        totalPrice += p.getPrice();
+    }
+
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    // Get the list of products
+    public List<Product> getProducts() {
+        return new ArrayList<>(products);
+    }
+
+    // Give view of summary
+    public String getSummary() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order for ").append(customer.getName()).append(":\n");
+        for (Product p : products) {
+            sb.append("- ").append(String.format("%.2f", totalPrice));
+            return sb.toString();
+        }
+    }
 }
